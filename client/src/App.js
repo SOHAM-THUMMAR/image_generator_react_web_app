bbee292fbe7dc488d34e81a7a4234771b659118a
SOHAM-THUMMAR <sohamthummar04@gmail.com>
2026-01-21 22:28:41 +0530

    home page and generatePost page are created and base theme has been applied and both file as route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,44 @@
-function App() {
-  return <div>Hellow!</div>;
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/theme";
+import { Home } from "./pages/home";
+import { GeneratePost } from "./pages/generatePost";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 3;
+`;
+
+export function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container>
+        <Wrapper>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} exact />
+              <Route path="/post" element={<GeneratePost />} exact />
+            </Routes>
+          </BrowserRouter>
+        </Wrapper>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ client/src/pages/generatePost.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const GeneratePost = () => {
+  return <div>generate post</div>;
+};
